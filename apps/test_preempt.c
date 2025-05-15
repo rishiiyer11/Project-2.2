3ad0ee00238c406ab4848f4cf7bfa6fb840b1107
Rishi Iyer <rishiiyer11@gmail.com>
2025-05-15 16:56:40 -0700

    re-enabled test_preempt, updated makefile, attempting to run

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -9,6 +9,7 @@
 Resources:
 https://www.geeksforgeeks.org/preemptive-and-non-preemptive-scheduling/
 https://www.gnu.org/software/libc/manual/2.36/html_mono/libc.html#Blocking-Signals
+https://stackoverflow.com/questions/3417837/best-way-to-suppress-c-warning-unused-variable-x
 */
 
 static volatile int busyCnt = 0;

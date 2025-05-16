54b129dfcd855c41bda1a16999c06b24f405b857
Rishi Iyer <rishiiyer11@gmail.com>
2025-05-15 18:56:58 -0700

    undid comment changes

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -14,10 +14,6 @@
 Resources:
 https://stackoverflow.com/questions/3417837/best-way-to-suppress-c-warning-unused-variable-x
 https://www.geeksforgeeks.org/preemptive-and-non-preemptive-scheduling/
-https://man7.org/linux/man-pages/man2/sigaction.2.html
-https://man7.org/linux/man-pages/man2/setitimer.2.html
-https://hpc-tutorials.llnl.gov/posix/
-https://pages.cs.wisc.edu/~remzi/Classes/537/Fall2008/Projects/p2a.html
 */
 
 /*
@@ -27,6 +23,57 @@ https://pages.cs.wisc.edu/~remzi/Classes/537/Fall2008/Projects/p2a.html
 #define HZ 100
 #define USEC_PER_SEC 1000000
 
+/*
+static struct sigaction old_action;
+static struct itimerval  old_timer;
+static sigset_t timer_mask;
+
+static void handle_timer(int sig)
+{
+	if (sig == SIGVTALRM) {
+		uthread_yield();
+	}
+}
+
+void preempt_disable(void)
+{
+	sigprocmask(SIG_BLOCK, &timer_mask, NULL);
+}
+
+void preempt_enable(void)
+{
+	sigprocmask(SIG_UNBLOCK, &timer_mask, NULL);
+}
+
+void preempt_start(bool preempt)
+{
+	if (!preempt) {
+		return;
+	}
+	sigemptyset(&timer_mask);
+    sigaddset(&timer_mask, SIGVTALRM);
+
+    // install our handler, saving the old one
+    struct sigaction sa;
+    sa.sa_handler = handle_timer;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    assert(sigaction(SIGVTALRM, &sa, &old_action) == 0);
+
+    // arm the virtual timer: first and recurring intervals
+    struct itimerval tv;
+    tv.it_interval.tv_sec  = 0;
+    tv.it_interval.tv_usec = USEC_PER_SEC / HZ;
+    tv.it_value = tv.it_interval;
+    assert(setitimer(ITIMER_VIRTUAL, &tv, &old_timer) == 0);
+}
+
+void preempt_stop(void)
+{
+	sigaction(SIGVTALRM, &old_action, NULL);
+    setitimer(ITIMER_VIRTUAL, &old_timer, NULL);
+}
+*/
 
 static struct sigaction ogAct;
 static struct itimerval  ogTime;
